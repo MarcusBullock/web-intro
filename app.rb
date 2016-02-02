7b3f1d3deb15d0015523438b335d630a733f54b4
JoeWroe <joe_wroe@icloud.com>
2016-02-02 11:09:54 +0000

    Challenge 8 Complete

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "Pissballs"
 end
@@ -11,4 +13,10 @@ end
 
 get '/secret' do
   "skjgsdoifhdsoigjsdog"
-end 
+end
+
+get '/cat' do
+  "<div style='border: 3px double red'>
+      <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
+   </div>"
+end
